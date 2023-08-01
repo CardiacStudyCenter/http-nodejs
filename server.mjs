b5b328a4fe8069ae734911cb356e6cb1050fc503
salmanafzal16 <salman.smo16@gmail.com>
2023-08-01 23:00:00 +0500

    Server Updated Again

--- server.mjs
+++ server.mjs
@@ -1,6 +1,45 @@
+'use strict';
 import { createServer } from 'http';
+import Client from 'ssh2-sftp-client';
+import csv from 'csvtojson';
+const sftp = new Client();
 
 createServer((req, res) => {
-  res.write('Hello World!');
-  res.end();
+  const config = {
+    host: 'sftp.paycomonline.net',
+    port: 22,
+    username: '000_0ry94_netsuite',
+    password: 'c^2Xx)6Lg^9So!2L'
+  };
+  sftp.connect(config)
+    .then(() => {
+      return sftp.cwd();
+    })
+    .then(() => {
+      return sftp.list('/Home/000_0ry94_netsuite/outbound');
+    })
+    .then(data => {
+      data.sort((a, b) => {
+        return b.modifyTime - a.modifyTime;
+      });
+      const latestFile = data[0].name;
+      console.log(latestFile);
+      return sftp.get(`/Home/000_0ry94_netsuite/outbound/${latestFile}`);
+    })
+    .then(data => {
+      // console.log(JSON.stringify(data));
+      return csv().fromString(data.toString());
+    })
+    .then((jsonObj) => {
+      // console.log(JSON.stringify(jsonObj));
+      res.write(JSON.stringify(jsonObj));
+      sftp.end();
+      res.end();
+    })
+    .catch(err => {
+      console.error(err.message);
+      res.write(err.message);
+      sftp.end();
+      res.end();
+    });
 }).listen(process.env.PORT);
